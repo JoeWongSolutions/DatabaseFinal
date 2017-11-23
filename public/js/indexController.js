d4e93a2609538d46f4f39a17aa0cf44700e8d453
Unknown <conrowland32@gmail.com>
2017-11-23 00:30:30 -0600

    Create contacts page UI

--- public/js/indexController.js
+++ public/js/indexController.js
@@ -1,13 +1,19 @@
 {
+	
+	var deleteTemp;
 
 	$(document).ready(function () {
 		$('.parallax').parallax();
-	});
-
-	$(document).ready(function () {
 		$('.modal').modal({
 			dismissible: false,
 			opacity: 0.8
+		});
+		$('.editButton').on('click', function() {
+			//TODO - edit function
+		});
+		$('.deleteButton').on('click', function() {
+			deleteTemp = $(this).closest('tr');
+			$('#confirmDeleteModal').modal('open');
 		});
 	});
 	
@@ -15,5 +21,33 @@
 		var currentLocation = window.location.href;
 		window.location = currentLocation + "contacts.html";
 	};
+	
+	var deleteContact = function(data) {
+		console.log(data);
+	};
+	
+	var deleteContact = function() {
+		//TODO - remove contact from DB
+		deleteTemp.remove();
+	};
+	
+	var testDBPull = function() {
+		console.log("Attempting to pull data from DB");
+		document.cookie = "id=1";
+		
+		$.ajax({
+			type: "GET",
+			url: "http://ec2-52-72-121-61.compute-1.amazonaws.com:3000",
+			xhrFields: {
+				withCredentials: true
+			},
+			crossDomain: true,
+			success: function(data) {
+				console.log("Success! The data is: " + data);
+			}
+		});
+	};
+	
+	testDBPull();
 
 }
