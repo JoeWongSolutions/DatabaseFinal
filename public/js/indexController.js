0bd8325b5b60be457215624bb723255615e409d0
Unknown <conrowland32@gmail.com>
2017-11-29 00:37:55 -0600

    11/29 changes
    Changes to server functionality, sign up functionality, added testing functions

--- public/js/indexController.js
+++ public/js/indexController.js
@@ -1,5 +1,5 @@
 {
-	
+
 	var deleteTemp;
 
 	$(document).ready(function () {
@@ -8,41 +8,106 @@
 			dismissible: false,
 			opacity: 0.8
 		});
-		$('.editButton').on('click', function() {
+		$('.editButton').on('click', function () {
 			//TODO - edit function
 		});
-		$('.deleteButton').on('click', function() {
+		$('.deleteButton').on('click', function () {
 			deleteTemp = $(this).closest('tr');
 			$('#confirmDeleteModal').modal('open');
 		});
 	});
-	
-	var loginSuccess = function() {
+
+	var loginSuccess = function () {
 		var currentLocation = window.location.href;
 		window.location = currentLocation + "contacts.html";
 	};
-	
-	var deleteContact = function(data) {
-		console.log(data);
-	};
-	
-	var deleteContact = function() {
+
+	var deleteContact = function () {
 		//TODO - remove contact from DB
 		deleteTemp.remove();
 	};
-	
-	var testDBPull = function() {
+
+	var testDupeUsername = function () {
+		$.ajax({
+			type: "GET",
+			url: "http://ec2-52-72-121-61.compute-1.amazonaws.com:3000/user",
+			data: {
+				userID: 3
+			},
+			success: function (result) {
+				console.log(result);
+			}
+		});
+	};
+
+	var createSubmit = function () {
+		var emailReg = /\S+@\S+\.\S+/;
+		var numberDupeUsernames = 0;
+
+		if ($('#firstName').val().length > 30) {
+			Materialize.toast("First name cannot be more than 30 characters.", 4000, 'red darken-2');
+		} else if ($('#lastName').val().length > 30) {
+			Materialize.toast("Last name cannot be more than 30 characters.", 4000, 'red darken-2');
+		} else if (emailReg.test($('#email').val()) == false) {
+			Materialize.toast("Please enter a valid email address.", 4000, 'red darken-2');
+		} else if ($('#createUsername').val().length < 6) {
+			Materialize.toast("Username must be at least 6 characters.", 4000, 'red darken-2');
+		} else if ($('#createUsername').val().length > 255) {
+			Materialize.toast("Username cannot be more than 255 characters.", 4000, 'red darken-2');
+		} else {
+			$.ajax({
+				type: "GET",
+				url: "http://ec2-52-72-121-61.compute-1.amazonaws.com:3000/user",
+				data: {
+					username: $('#createUsername').val()
+				},
+				success: function (result) {
+					numberDupeUsernames = result[0]["COUNT(id)"];
+				}
+			}).then(function () {
+				if (numberDupeUsernames != 0) {
+					Materialize.toast("Username is already taken.", 4000, 'red darken-2');
+				} else if ($('#createPassword').val().length < 6) {
+					Materialize.toast("Password must be at least 6 characters.", 4000, 'red darken-2');
+				} else if ($('#createPassword').val().length > 255) {
+					Materialize.toast("Password cannot be more than 255 characters.", 4000, 'red darken-2');
+				} else if ($('#confirmPassword').val() != $('#createPassword').val()) {
+					Materialize.toast("Passwords do not match.", 4000, 'red darken-2');
+				} else {
+					$.ajax({
+						type: "POST",
+						url: "http://ec2-52-72-121-61.compute-1.amazonaws.com:3000/user",
+						data: {
+							username: $('#createUsername').val(),
+							password: $('#createPassword').val(),
+							fname: $('#firstName').val(),
+							lname: $('#lastName').val(),
+							email: $('#email').val()
+						},
+						success: function (result) {
+							
+						}
+					});
+					Materialize.toast("User created. Welcome!", 4000, 'green');
+					$('#signUpModal').modal('close');
+				}
+			});
+		}
+	};
+
+
+
+	var testDBPull = function () {
 		console.log("Attempting to pull data from DB");
-		
+
 		$.ajax({
 			type: "GET",
 			url: "http://ec2-52-72-121-61.compute-1.amazonaws.com:3000/contacts",
 			data: {
 				userID: 1
 			},
-			crossDomain: true,
-			success: function(data) {
-				console.log("Success! The data is: " + data);
+			success: function (result) {
+				console.log(result);
 			}
 		});
 	};
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 /* 
-* Joe Wong
-* CS3380 Final Project
-* 11/10/2017
-*/
+ * Joe Wong
+ * CS3380 Final Project
+ * 11/10/2017
+ */
 
 // We are importing the libraries we need for our server
 const express = require('express');
@@ -11,141 +11,192 @@ const app = express();
 const mysql = require('mysql');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
 
 // We declare what tools can be used with our app
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	next();
 });
 
 // We need to connect to Mysqldb
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "web",
-  password: "web",
-  database: "contactlist"
+	connectionLimit: 10,
+	host: "localhost",
+	user: "web",
+	password: "web",
+	database: "contactlist"
 });
 
 var con;
 var cookies;
 
 //This is where authentication takes place
-app.post('/login', (req,res) => {
-    console.log("Reached login function.");
-    //connect to the database
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      else {
-//          con = connection;
-          console.log("Connected! from post-login");
-    
-        let username = req.body.username;
-        let pass = req.body.password;
-        let sql = "SELECT id FROM users WHERE username=? AND password=?"; 
-        connection.query(
-            sql, 
-            [username,pass],
-            (err, result) => {
-                if(err) {
-                    console.log("There was a MYSQL error: "+err);
-                    res.redirect("/");
-                }
-                else{
-                    console.log("Checked Authentication for "+result[0].id);
-                    res.cookie('userID',result[0].id, {httpOnly: false});
-                    console.log("successfully set cookie");
-                    res.send({message: "Successfully set cookie"});
-                }
-            }
-        );
-        connection.release();
-      }
-    });
+app.post('/login', (req, res) => {
+	console.log("Reached login function.");
+	//connect to the database
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		else {
+			//          con = connection;
+			console.log("Connected! from post-login");
+
+			let username = req.body.username;
+			let pass = req.body.password;
+			let sql = "SELECT id FROM users WHERE username=? AND password=?";
+			connection.query(
+				sql, [username, pass],
+				(err, result) => {
+					if (err) {
+						console.log("There was a MYSQL error: " + err);
+						res.redirect("/");
+					} else {
+						console.log("Checked Authentication for " + result[0].id);
+						res.cookie('userID', result[0].id, {
+							httpOnly: false
+						});
+						console.log("successfully set cookie");
+						res.send({
+							message: "Successfully set cookie"
+						});
+					}
+				}
+			);
+			connection.release();
+		}
+	});
 });
 
 // This is where we define CRUD for users
-app.get('/user',(req,res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    //connect to the database
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      else{
-        console.log("Connected! from get-user");    
-        
-        //get any cookies from the request 
-        let userID = req.cookies.userID;
 
-        //query the db
-        if(userID){
-            let sql = "SELECT * FROM users WHERE id =" + userID;
-            connection.query(sql,(err, result) => {
-                if(err) return console.log(err);
-                else res.send(result);
-            });
-        }
-        else {
-            //redirect to the login
-            res.redirect("/");
-        }
-        connection.release();
-      }
-    });
+app.get('/userAll', (req, res) => {
+	//connect to the database
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		else {
+			console.log("Connected! from get-user");
+
+			//query the db
+			if (true) {
+				let sql = "SELECT * FROM users";
+				connection.query(sql, (err, result) => {
+					if (err) return console.log(err);
+					else res.send(result);
+				});
+			} else {
+				//redirect to the login
+			}
+			connection.release();
+		}
+	});
 });
 
-app.post('/user',(req,res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      else{
-        console.log("Connected!");
-        let user = req.body;
-        let sql = "INSERT INTO users (username, password, fname, lname, email, joined) values(?,?,?)"; 
-        connection.query(
-            sql, 
-            [ 
+app.get('/userDesc', (req, res) => {
+	//connect to the database
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		else {
+			console.log("Connected! from get-user");
+
+			//query the db
+			if (true) {
+				let sql = "DESCRIBE users";
+				connection.query(sql, (err, result) => {
+					if (err) return console.log(err);
+					else res.send(result);
+				});
+			} else {
+				//redirect to the login
+			}
+			connection.release();
+		}
+	});
+});
+
+app.get('/user', (req, res) => {
+	if (req.query.username == null && req.query.userID == null) {
+		res.send("No userID or username");
+		return;
+	}
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		else {
+			console.log("Connected! from get-user");
+
+			if (req.query.username != null) {
+				let username = req.query.username;
+				let sql = "SELECT COUNT(id) FROM users WHERE username = '" + username + "'";
+				connection.query(sql, (err, result) => {
+					if (err) res.send(err);
+					else res.send(result);
+				});
+				connection.release();
+			} else {
+				let userID = req.query.userID;
+				let sql = "SELECT * FROM users WHERE id = " + userID;
+				connection.query(sql, (err, result) => {
+					if (err) res.send(err);
+					else res.send(result);
+				});
+				connection.release();
+			}
+		}
+	});
+});
+
+app.post('/user', (req, res) => {
+	pool.getConnection(function (err, connection) {
+		if (err) res.send("Error: " + err);
+		else {
+			console.log("Connected!");
+			let user = req.body;
+			var salt = crypto.randomBytes(Math.ceil(32)).toString('hex').slice(0, 64);
+			var hashedPass = crypto.createHash('sha256').update(user.password + salt).digest('hex');
+
+			let sql = "INSERT INTO users (username, password, fname, lname, email, joined) values(?,?,?,?,?,?)";
+			connection.query(
+				sql, [
                 user.username,
-                user.password,
+                hashedPass,
                 user.fname,
                 user.lname,
                 user.email,
-                Date.now(),
+                new Date().toISOString().slice(0, 19).replace('T', ' '),
             ],
-            (err, result) => {
-                if(err) return console.log(err);
-                else{
-                    console.log("Saved to database");
-                    res.send({message: req.body.fname + " was successfully added to the database"});
-                }
-            }
-        );          
-      }
-      connection.release();
-    });    
+				(err, result) => {
+					if (err) res.send(err);
+					else {
+						console.log("Saved to database");
+						res.send({
+							message: req.body.fname + " was successfully added to the database"
+						});
+					}
+				}
+			);
+		}
+		connection.release();
+	});
 });
 
 app.put('/user', (req, res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-        console.log("Connected!");
-        let user = req.body;
-        let sql = "UPDATE users SET username=?, password=?, fname=?, lname=?, email=? WHERE id=?";
-        connection.query(
-            sql,
-            [
+	if (!req.cookies.userID) {
+		return;
+	}
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		console.log("Connected!");
+		let user = req.body;
+		let sql = "UPDATE users SET username=?, password=?, fname=?, lname=?, email=? WHERE id=?";
+		connection.query(
+			sql, [
                 user.username,
                 user.password,
                 user.fname,
@@ -153,95 +204,95 @@ app.put('/user', (req, res) => {
                 user.email,
                 req.cookies.userID
             ],
-            (err, result) => {
-                if(err) return res.send(err);
-                res.send(result);
-            }
-        );
-        connection.release();        
-    });
+			(err, result) => {
+				if (err) return res.send(err);
+				res.send(result);
+			}
+		);
+		connection.release();
+	});
 });
 
 app.delete('/user*', (req, res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      console.log("Connected!");
-        let sql = "DELETE FROM users WHERE id=?";
-        if(req.query.id == "all"){
-            connection.query(
-                "DELETE FROM users",
-                (err, result) => {
-                    if(err) return res.send(err);
-                    res.send({message: result + " was deleted successfully"});
-                }
-            );
-        }
-        else {
-            connection.query(
-                sql,
-                [req.cookies.userID],
-                (err, result) => {
-                    if(err) return res.send(err);
-                    else{
-                        res.clearCookie("userID");
-                        res.send({message: result + " was deleted successfully"});                        
-                    }
-                }    
-            );
-        }
-        connection.release();        
-    });
+	if (!req.query.userID) {
+		res.send("No userID");
+		return;
+	}
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		console.log("Connected!");
+		let sql = "DELETE FROM users WHERE id=?";
+		//        if(req.query.id == "all"){
+		//            connection.query(
+		//                "DELETE FROM users",
+		//                (err, result) => {
+		//                    if(err) return res.send(err);
+		//                    res.send({message: result + " was deleted successfully"});
+		//                }
+		//            );
+		//        }
+		//        else {
+		connection.query(
+			sql, [req.query.userID],
+			(err, result) => {
+				if (err) return res.send(err);
+				else {
+					res.send({
+						message: result + " was deleted successfully"
+					});
+				}
+			}
+		);
+		//        }
+		connection.release();
+	});
 });
 
 
 // This is where we define our CRUD listeners for contacts
 
 app.get('/contacts', (req, res) => {
-    if(!req.body.userID){
-        return;
-    }
-    //connect to the database
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      else{
-        console.log("Connected! from get-contacts");    
-        
-        //get any cookies from the request 
-        let userID = req.body.userID;
+	if (!req.query.userID) {
+		res.send("No userID");
+		return;
+	}
+	//connect to the database
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		else {
+			console.log("Connected! from get-contacts");
 
-        //query the db
-        if(userID){
-            let sql = "SELECT * FROM contacts WHERE userID =" + userID;
-            connection.query(sql,(err, result) => {
-                if(err) return console.log(err);
-                else res.send(result);
-            });
-        }
-        else {
-            //redirect to the login
-            res.redirect("/");
-        }
-        connection.release();
-      }
-    });
+			//get any cookies from the request 
+			let userID = req.query.userID;
+
+			//query the db
+			if (userID) {
+				let sql = "SELECT * FROM contacts WHERE userID =" + userID;
+				connection.query(sql, (err, result) => {
+					if (err) return console.log(err);
+					else res.send(result);
+				});
+			} else {
+				res.send("User not found.");
+				//redirect to login
+			}
+			connection.release();
+		}
+	});
 });
 
 app.post('/contacts', (req, res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      else{
-        console.log("Connected!");
-        let contact = req.body;
-        let sql = "INSERT INTO contacts (userID, fname, lname, company, phone, street, zip, email) values(?,?,?,?,?,?,?,?)"; 
-        connection.query(
-            sql, 
-            [ 
+	if (!req.cookies.userID) {
+		return;
+	}
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		else {
+			console.log("Connected!");
+			let contact = req.body;
+			let sql = "INSERT INTO contacts (userID, fname, lname, company, phone, street, zip, email) values(?,?,?,?,?,?,?,?)";
+			connection.query(
+				sql, [
                 req.cookies.userID,
                 contact.fname,
                 contact.lname,
@@ -251,31 +302,32 @@ app.post('/contacts', (req, res) => {
                 contact.zip,
                 contact.email
             ],
-            (err, result) => {
-                if(err) return console.log(err);
-                else{
-                    console.log("Saved to database");
-                    res.send({message: req.body.fname + " was successfully added to the database"});
-                }
-            }
-        );          
-      }
-      connection.release();
-    });
+				(err, result) => {
+					if (err) return console.log(err);
+					else {
+						console.log("Saved to database");
+						res.send({
+							message: req.body.fname + " was successfully added to the database"
+						});
+					}
+				}
+			);
+		}
+		connection.release();
+	});
 });
 
 app.put('/contacts', (req, res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-        console.log("Connected!");
-        let contact = req.body;
-        let sql = "UPDATE contacts SET fname=?, lname=?, company=?, phone=?, street=?, zip=?, email=? WHERE id=?";
-        connection.query(
-            sql,
-            [
+	if (!req.cookies.userID) {
+		return;
+	}
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		console.log("Connected!");
+		let contact = req.body;
+		let sql = "UPDATE contacts SET fname=?, lname=?, company=?, phone=?, street=?, zip=?, email=? WHERE id=?";
+		connection.query(
+			sql, [
                 contact.fname,
                 contact.lname,
                 contact.company,
@@ -285,49 +337,51 @@ app.put('/contacts', (req, res) => {
                 contact.email,
                 contact.id
             ],
-            (err, result) => {
-                if(err) return res.send(err);
-                res.send(result);
-            }
-        );
-        connection.release();        
-    });
+			(err, result) => {
+				if (err) return res.send(err);
+				res.send(result);
+			}
+		);
+		connection.release();
+	});
 });
 
 app.delete('/contacts*', (req, res) => {
-    if(!req.cookies.userID){
-        return;
-    }
-    pool.getConnection(function(err,connection) {
-      if (err) throw err;
-      console.log("Connected!");
-        let sql = "DELETE FROM contacts WHERE id=?";
-        if(req.query.id == "all"){
-            connection.query(
-                "DELETE FROM contacts",
-                (err, result) => {
-                    if(err) return res.send(err);
-                    res.send({message: result + " was deleted successfully"});
-                }
-            );
-        }
-        else {
-            connection.query(
-                sql,
-                [req.query.id],
-                (err, result) => {
-                    if(err) return res.send(err);
-                    res.send({message: result + " was deleted successfully"});
-                }    
-            );
-        }
-        connection.release();        
-    });
+	if (!req.cookies.userID) {
+		return;
+	}
+	pool.getConnection(function (err, connection) {
+		if (err) throw err;
+		console.log("Connected!");
+		let sql = "DELETE FROM contacts WHERE id=?";
+		if (req.query.id == "all") {
+			connection.query(
+				"DELETE FROM contacts",
+				(err, result) => {
+					if (err) return res.send(err);
+					res.send({
+						message: result + " was deleted successfully"
+					});
+				}
+			);
+		} else {
+			connection.query(
+				sql, [req.query.id],
+				(err, result) => {
+					if (err) return res.send(err);
+					res.send({
+						message: result + " was deleted successfully"
+					});
+				}
+			);
+		}
+		connection.release();
+	});
 });
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function () {
-    console.log('Listening on Port 3000');
+	console.log('Listening on Port 3000');
 });
